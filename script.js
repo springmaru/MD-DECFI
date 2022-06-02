fa679ccb728e19ce80f35eb657bc4a6a5131e3b8
springmaru <springmaru7@gmail.com>
2022-06-02 22:04:34 +0900

    이불킥 할만한거 삭제

--- script.js
+++ script.js
@@ -14,8 +14,9 @@ for (i in list){console.log(i)}
 
 
 
-// 개수도 세야하는데..?
 
+// 개수도 세야하는데..?
+// 
 
 function get_probability(symptoms){
     let expected_symptoms = {};
@@ -34,9 +35,8 @@ function get_probability(symptoms){
 
 
 for (i in get_probability()){
-    // 나 지금 심정이 약간 긱사 들어가고 싫고 들어가지 않는것도 싫고 집 가기도 싫고 집 가지 않는 것도 싫어
-    // 제주도 가면 나 맛있는거 먹음? 아 진짜? 비행기 얼마.
+
     // 
 
-    
+
 }
